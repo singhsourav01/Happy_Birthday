fa76a1e677dbdee7e3e198085c2e70a0cacb9db8
singhsourav01 <singhsourav0110@gmail.com>
2026-08-20 14:50:59 +0530

    Done

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  // base: "/mummy-birthday/",
+  base: "/Happy_Birthday/",
   server: {
     host: "0.0.0.0",
     port: 4173,
